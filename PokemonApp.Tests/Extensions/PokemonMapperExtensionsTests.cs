1b9f2e942f47fd1c2c1937142a7f27ddb993047d
Daniella Paal <Daniella.Paal@zonal.co.uk>
2025-07-08 22:29:39 +0100

    Adding PokemonMapperExtensions tests

---
+++ PokemonApp.Tests/Extensions/PokemonMapperExtensionsTests.cs
@@ -0,0 +1,34 @@
+﻿using PokemonApp.Server.Extensions;
+
+namespace PokemonApp.Tests.Extensions
+{
+    public class PokemonMapperExtensionsTests
+    {
+        [Fact]
+        public void ExtractIdFromUrl_ValidUrl_ReturnsId()
+        {
+            string url = "https://pokeapi.co/api/v2/ability/65/";
+
+            int id = PokemonMapperExtensions.ExtractIdFromUrl(url);
+
+            Assert.Equal(65, id);
+        }
+
+        [Fact]
+        public void ExtractIdFromUrl_InvalidUrl_ThrowsException()
+        {
+            string url = "https://pokeapi.co/api/v2/ability/notanumber/";
+
+            var ex = Assert.Throws<Exception>(() => PokemonMapperExtensions.ExtractIdFromUrl(url));
+            Assert.Equal("Invalid URL: https://pokeapi.co/api/v2/ability/notanumber/", ex.Message);
+        }
+
+        [Fact]
+        public void ExtractIdFromUrl_NullUrl_ThrowsArgumentNullException()
+        {
+            string? url = null;
+
+            Assert.Throws<ArgumentNullException>(() => PokemonMapperExtensions.ExtractIdFromUrl(url));
+        }
+    }
+}
